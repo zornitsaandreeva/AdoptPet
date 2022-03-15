360e8dd2aab5fe7290bf90dd8fe45e8fc674b304
zornitsaandreeva <zornichka92@gmail.com>
2022-03-15 13:20:31 +0000

    Work on AdoptPet

--- Adopt/Data/ApplicationDbContext.cs
+++ Adopt/Data/ApplicationDbContext.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Adopt.Data
+﻿namespace Adopt.Data
 {
+    using Adopt.Models;
+    using AdoptPet.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Breed> Breeds { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CurrentHome> CurrentHomes { get; set; }
+        public DbSet<Image> Images { get; set; }
     }
 }
